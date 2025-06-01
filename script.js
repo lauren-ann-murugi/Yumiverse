fa6dad79e2a29db431f0d11a1fcbdb5f60749455
laurenannmurugi <laurenann1871@gmail.com>
2025-06-01 21:33:56 +0300

    My first commit

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+//const h2 = document.createElement("h2");
+document.getElementById("p1").textContent =`Dive into a world of delicious discoveries ,from cozy home cooked meals to global dishes, name it all.`;
+document.getElementById("p2").textContent =`We celebrate the joy of the food in every form. You are in the right place.`;
+document.getElementById("p3").textContent =`Our goal is to create a warm space where people can eat, connect, and enjoy.`;
+document.getElementById("p4").textContent =`Brings you flavors of life through delicious meals`;
+document.getElementById("p5").textContent =`Come dine with us and taste the world on a plate!`;
+document.getElementById("p6").textContent =`Skilled in customer service and hotel operations`;
+document.getElementById("p7").textContent =`Skilled in food preparation and kitchen management`;
+document.getElementById("p8").textContent =`Experienced in serving guests and handling orders`;
+document.getElementById("p9").textContent =`Proficient in front desk and support`;
+document.getElementById("p10").textContent =`Organized weekly cultural food nights`;
+document.getElementById("p11").textContent =`Organized 'flavours of life' food fasting events`;
+
+//h2.textContent = "This content added by JavaScript";
+//document.querySelector("body").appendChild(h2);
